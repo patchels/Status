a184b350cf5eb05f92ba2536a4cbd36b0ee58097
patchels <122473854+patchels@users.noreply.github.com>
2023-02-22 12:51:41 -0500

    Add files via upload

---
+++ Status.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+import java.util.Scanner;
+
+/** This class takes String input from user 
+ * and calls method statusDetail to print the 
+ * status of User-Rejected,Pending,Processing and Approved.
+ * if the input is not given in String, 
+ * program will accept and produce wrong results. 
+ * change the code to use enums which avoids String input 
+ * then print the status details. 
+ * @author srinivsi 
+ */
+public class Status {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    Scanner in =new Scanner(System.in);
+    System.out.println("Enter the user status code (zero,one,two,three) in string");
+    String code = in.next();
+    StausUser t= new StausUser();
+    t.statusDetail(code); 
+    }
+    
+}
---
+++ StausUser.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+/**
+ * MidTerm-Exam
+ * @author srinivsi
+ */
+public class StausUser 
+{
+    private enum UserStatus {
+        REJECTED,
+        PENDING,
+        PROCESSING,
+        APPROVED
+    }
+   public void statusDetail(String code)
+{
+switch(code.toUpperCase())
+        {
+        case "ZERO": System.out.println("REJECTED");
+        break;
+        case "ONE": System.out.println("PENDING");
+        break;
+        case "TWO":
+        System.out.println("PROCESSING");
+        break;
+        case "THREE": 
+            System.out.println("APPROVED");
+        break;
+        default:
+         System.out.println("NOT VALID CODE");
+        break;
+        }
+}
+ 
+}
